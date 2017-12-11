bc9aec01c6b21b5af37a50b4c3727a5dc857c614
Fredrik Arenhag <fredrik.arenhag@diktator.org>
2017-12-11 13:30:32 +0100

    Made the new data contain the right number of laps.

--- GarminLaps/Recalculators/LapRecalculator.cs
+++ GarminLaps/Recalculators/LapRecalculator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using GarminLaps.Dto;
 
 namespace GarminLaps.Recalculators
@@ -8,7 +9,19 @@ namespace GarminLaps.Recalculators
     {
         public LapData RedistributeLaps(LapData oldLapDto, List<TimeSpan> durationsForNewLaps)
         {
-            return oldLapDto;
+            var newLapDto = new LapData();
+
+            foreach (var timedLap in durationsForNewLaps)
+            {
+                newLapDto.Laps.Add(new Lap());
+            }
+
+            newLapDto.Laps.Add(new Lap());
+            
+            var sumOfCalories = oldLapDto.Laps.Sum(a => a.Calories);
+            var sumOfHeartRates = oldLapDto.Laps.SelectMany(a => a.TrackPoints).Sum(b => b.HeartRateBpm);
+
+            return newLapDto;
         }
     }
 }
--- GarminLapsTests/RecalculationTests/LapRecalculatorTests.cs
+++ GarminLapsTests/RecalculationTests/LapRecalculatorTests.cs
@@ -9,11 +9,11 @@ namespace GarminLapsTests.RecalculationTests
     public class LapRecalculatorTests
     {
         [Fact]
-        public void Should_something()
+        public void Should_return_correct_number_of_laps()
         {
             // Arrange
             var lapRecalculator = new LapRecalculator();
-            var oldLapDto = SetupFakeLapData(2, 1000);
+            var oldLapDto = FakeLapSetup.SetupFakeLapData(2, 1000);
 
             var durationsForNewLaps = new List<TimeSpan>()
             {
@@ -26,57 +26,15 @@ namespace GarminLapsTests.RecalculationTests
                 new TimeSpan(0,0,12),
                 new TimeSpan(0,0,14)
             };
-            
-            LapData newLapDto = lapRecalculator.RedistributeLaps(oldLapDto, durationsForNewLaps);
 
             // Act
-            // 2) tell "something" to redistribute the laps according to a new given pattern
+            // 2) tell the recalculator to redistribute the laps according to a new given pattern
                 // make this persist in the DTO model
                 // check that this "something" has performed correct calculations regarding heart rates and calories
+            LapData newLapDto = lapRecalculator.RedistributeLaps(oldLapDto, durationsForNewLaps);
 
             // Assert
-            Assert.Equal(durationsForNewLaps.Count, newLapDto.Laps.Count + 1);
-        }
-
-        private LapData SetupFakeLapData(int numberOfLaps, int numberOfTrackPointsPerLap)
-        {
-            var lapDataToReturn = new LapData();
-            
-            for (int i = 0; i < numberOfLaps; i++)
-            {
-                lapDataToReturn.Laps.Add(NewMethod(ReturnTrackPoints(numberOfTrackPointsPerLap)));
-            }
-
-            return lapDataToReturn;
-        }
-
-        private static Lap NewMethod(List<TrackPoint> aBunchOfTrackPoints)
-        {
-            return new Lap()
-            {
-                Calories = 123,
-                TrackPoints = aBunchOfTrackPoints
-            };
-        }
-
-        private static List<TrackPoint> ReturnTrackPoints(int numberOfTrackPoints)
-        {
-            var aBunchOfTrackPoints = new List<TrackPoint>();
-            var startTime = DateTime.Now;
-
-            for (int i = 0; i < numberOfTrackPoints; i++)
-            {
-                aBunchOfTrackPoints.Add(new TrackPoint()
-                {
-                    AltitudeMeters = 100,
-                    DistanceMeters = 20,
-                    HeartRateBpm = 110,
-                    SensorState = SensorState.Present,
-                    DateTime = startTime.AddSeconds(i)
-                });
-            }
-
-            return aBunchOfTrackPoints;
+            Assert.Equal(durationsForNewLaps.Count + 1, newLapDto.Laps.Count);
         }
     }
 }
