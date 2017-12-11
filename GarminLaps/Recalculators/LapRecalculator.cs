a8aaee1f407d58a1163310f72a3493bb25450dfd
Fredrik Arenhag <fredrik.arenhag@diktator.org>
2017-12-11 15:20:16 +0100

    Finished the track point transplantation.

--- GarminLaps/Recalculators/LapRecalculator.cs
+++ GarminLaps/Recalculators/LapRecalculator.cs
@@ -10,16 +10,38 @@ namespace GarminLaps.Recalculators
         public LapData RedistributeLaps(LapData oldLapDto, List<TimeSpan> durationsForNewLaps)
         {
             var newLapDto = new LapData();
+            var allTrackPoints = oldLapDto.Laps.SelectMany(a => a.TrackPoints);
+
+            var upcomingLapStartTime = oldLapDto.Laps[0].TrackPoints[0].DateTime.Subtract(new TimeSpan(1)); // remove one tick to make sure that the first track point of all gets included
 
             foreach (var timedLap in durationsForNewLaps)
             {
-                newLapDto.Laps.Add(new Lap());
+                var upcomingLapEndTime = upcomingLapStartTime.Add(timedLap);
+                var trackPointsToTransplantToNewLap = allTrackPoints.Where(a => a.DateTime > upcomingLapStartTime && a.DateTime <= upcomingLapEndTime);
+
+                newLapDto.Laps.Add(new Lap()
+                {
+                    TrackPoints = trackPointsToTransplantToNewLap.ToList()
+                });
+
+                upcomingLapStartTime = upcomingLapEndTime;
             }
 
-            newLapDto.Laps.Add(new Lap());
-            
-            var sumOfCalories = oldLapDto.Laps.Sum(a => a.Calories);
-            var sumOfHeartRates = oldLapDto.Laps.SelectMany(a => a.TrackPoints).Sum(b => b.HeartRateBpm);
+            // Make sure that any track points not covered by the stated lap times are appended to a last lap
+            var remainingTrackPoints = allTrackPoints.Where(a => a.DateTime > upcomingLapStartTime);
+
+            if (remainingTrackPoints.Count() > 0)
+            {
+                var trackPointsToTransplantToNewLap = allTrackPoints.Where(a => a.DateTime >= upcomingLapStartTime);
+
+                newLapDto.Laps.Add(new Lap()
+                {
+                    TrackPoints = trackPointsToTransplantToNewLap.ToList()
+                });
+            }
+
+            //var sumOfCalories = oldLapDto.Laps.Sum(a => a.Calories);
+            //var sumOfHeartRates = oldLapDto.Laps.SelectMany(a => a.TrackPoints).Sum(b => b.HeartRateBpm);
 
             return newLapDto;
         }
--- GarminLapsTests/RecalculationTests/LapRecalculatorTests.cs
+++ GarminLapsTests/RecalculationTests/LapRecalculatorTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using GarminLaps.Dto;
 using GarminLaps.Recalculators;
 using Xunit;
@@ -28,13 +29,67 @@ namespace GarminLapsTests.RecalculationTests
             };
 
             // Act
-            // 2) tell the recalculator to redistribute the laps according to a new given pattern
-                // make this persist in the DTO model
-                // check that this "something" has performed correct calculations regarding heart rates and calories
             LapData newLapDto = lapRecalculator.RedistributeLaps(oldLapDto, durationsForNewLaps);
 
             // Assert
             Assert.Equal(durationsForNewLaps.Count + 1, newLapDto.Laps.Count);
+        }
+
+        [Fact]
+        public void Should_not_remove_nor_add_any_track_points()
+        {
+            // Arrange
+            var lapRecalculator = new LapRecalculator();
+            var oldLapDto = FakeLapSetup.SetupFakeLapData(2, 1000);
+
+            var durationsForNewLaps = new List<TimeSpan>()
+            {
+                new TimeSpan(0,0,14),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,14),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,14),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,14)
+            };
+
+            // Act
+            LapData newLapDto = lapRecalculator.RedistributeLaps(oldLapDto, durationsForNewLaps);
+
+            var expected = oldLapDto.Laps.SelectMany(a => a.TrackPoints).Count();
+            var actual = newLapDto.Laps.SelectMany(a => a.TrackPoints).Count();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_return_atleast_one_track_point_in_all_laps()
+        {
+            // Arrange
+            var lapRecalculator = new LapRecalculator();
+            var oldLapDto = FakeLapSetup.SetupFakeLapData(2, 1000);
+
+            var durationsForNewLaps = new List<TimeSpan>()
+            {
+                new TimeSpan(0,0,14),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,14),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,14),
+                new TimeSpan(0,0,12),
+                new TimeSpan(0,0,14)
+            };
+
+            // Act
+            LapData newLapDto = lapRecalculator.RedistributeLaps(oldLapDto, durationsForNewLaps);
+            var expected = newLapDto.Laps.Count();
+            var actual = newLapDto.Laps.Where(a => a.TrackPoints.Count > 0).Count();
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
